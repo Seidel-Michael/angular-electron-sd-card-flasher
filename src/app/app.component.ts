b22d9787016dc1edeea0715d6d7516b3071c7167
Fabian Beyerlein <mail@fabian-b.de>
2017-12-22 10:23:12 +0100

    updated packages created flash component and wait page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,11 @@
-import { Component } from '@angular/core';
-import { ElectronService } from './providers/electron.service';
-import { TranslateService } from '@ngx-translate/core';
+import {Component} from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
-})
+import {ElectronService} from './providers/electron.service';
+
+@Component({selector: 'app-root', templateUrl: './app.component.html', styleUrls: ['./app.component.scss']})
 export class AppComponent {
-  constructor(public electronService: ElectronService,
-    private translate: TranslateService) {
-
-    translate.setDefaultLang('en');
-
+  constructor(public electronService: ElectronService) {
     if (electronService.isElectron()) {
       console.log('Mode electron');
       // Check if electron is correctly injected (see externals in webpack.config.js)
